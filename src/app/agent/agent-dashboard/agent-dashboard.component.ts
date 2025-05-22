5ef89267719d9ed816878eb0c14afd58804f7d78
arij-nabli <115657090+arij-nabli@users.noreply.github.com>
2025-05-22 10:22:47 +0200

    noootif_push_commit1

--- src/app/agent/agent-dashboard/agent-dashboard.component.ts
+++ src/app/agent/agent-dashboard/agent-dashboard.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NotificationService } from '../notification.service';
 
 @Component({
   selector: 'app-agent-dashboard',
   templateUrl: './agent-dashboard.component.html',
   styleUrls: ['./agent-dashboard.component.css']
 })
-export class AgentDashboardComponent {
+export class AgentDashboardComponent implements OnInit {
+  
+  constructor(private notificationService: NotificationService) {}
 
+  ngOnInit() {
+    // Test des notifications après 2 secondes
+    setTimeout(() => {
+      this.notificationService.testNotification();
+    }, 2000);
+  }
 }
